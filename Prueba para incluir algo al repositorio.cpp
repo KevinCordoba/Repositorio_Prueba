5c0ef75434ae5684a8882553e2b7d055439b110b
Kevin <cordobakevin21@estudiantec.cr>
2021-02-21 17:43:36 -0600

    Archivo c++
    Intento de incluir un archivo de c++ en el repositorio.

---
+++ Prueba para incluir algo al repositorio.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	int a,b,resultado=0;
+	cout<<"Ingrese el valor de a: ";cin>>a;
+	cout<<"Ingrese el valor de b: ";cin>>b;
+	resultado=a+b;
+	cout<<"La suma de estos enteros es: "<<resultado;
+	
+	getch();
+	return 0;
+}
